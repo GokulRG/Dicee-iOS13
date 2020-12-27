45eaca49c63983e4a37a7acee7a277ddb4ac199c
GokulRG <ramakrishnan.go@husky.neu.edu>
2020-12-27 04:17:45 -0800

    making final edits to the project with random number selection

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IBOutlets added by Ctrl+click and drag to the assistant
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // diceImageViewOne.image = diceImages[Int.random(in: 0...5)]
+        // diceImageViewTwo.image = diceImages[Int.random(in: 0...5)]
+        
+        // Even more succint -- Gives a random element from the array
+        diceImageViewOne.image = diceImages.randomElement();
+        diceImageViewTwo.image = diceImages.randomElement();
     }
-
-
 }
+
 
